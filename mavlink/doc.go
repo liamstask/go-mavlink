c3a7ce8113fabeb236eb71a1ae6c542df5874621
Liam Staskawicz <liam@stask.net>
2015-05-29 15:31:45 -0700

    doc.go: prelim godoc support

---
+++ mavlink/doc.go
@@ -0,0 +1,13 @@
+
+/*
+Package mavlink provides support for encoding/decoding mavlink (http://qgroundcontrol.org/mavlink/start) messages.
+
+Encoding is done via the Encoder type, which wraps an io.Writer, and decoding is done via the Decoder type, which wraps an io.Reader.
+
+The Packet type represents the wire type used for message transmission/reception, and a variety of generated classes each implement the Message type.
+A Message can be converted to a Packet by calling Message.Pack(p) and a Packet can be converted to a Message by calling Message.Unpack(p).
+
+See the ReadMe for examples of typical usage.
+
+*/
+package mavlink
